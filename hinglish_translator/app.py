1cc889aaa12c87715fb54d44b6584ac0decc044b
azuredzkj <60534713+azuredzkj@users.noreply.github.com>
2024-10-07 16:30:37 +0530

    Add files via upload

---
+++ hinglish_translator/app.py
@@ -0,0 +1,174 @@
+from flask import Flask, render_template, request
+from googletrans import Translator
+
+app = Flask(__name__)
+
+# Predefined dictionary of supported languages
+LANGUAGES = {
+    'choose':'select_language',
+    'hi': 'hindi',
+    'en': 'english',
+    # 'af': 'afrikaans',
+    'sq': 'albanian',
+    # 'am': 'amharic',
+    'ar': 'arabic',
+    'hy': 'armenian',
+    'az': 'azerbaijani',
+    'eu': 'basque',
+    'be': 'belarusian',
+    'bn': 'bengali',
+    'bs': 'bosnian',
+    'bg': 'bulgarian',
+    'ca': 'catalan',
+    'ceb': 'cebuano',
+    'ny': 'chichewa',
+    'zh-cn': 'chinese (simplified)',
+    'zh-tw': 'chinese (traditional)',
+    'co': 'corsican',
+    'hr': 'croatian',
+    'cs': 'czech',
+    'da': 'danish',
+    'nl': 'dutch',
+    
+    'eo': 'esperanto',
+    'et': 'estonian',
+    'tl': 'filipino',
+    'fi': 'finnish',
+    'fr': 'french',
+    'fy': 'frisian',
+    'gl': 'galician',
+    'ka': 'georgian',
+    'de': 'german',
+    'el': 'greek',
+    'gu': 'gujarati',
+    'ht': 'haitian creole',
+    'ha': 'hausa',
+    'haw': 'hawaiian',
+    'iw': 'hebrew',
+    'he': 'hebrew',
+    
+    'hmn': 'hmong',
+    'hu': 'hungarian',
+    'is': 'icelandic',
+    'ig': 'igbo',
+    'id': 'indonesian',
+    'ga': 'irish',
+    'it': 'italian',
+    'ja': 'japanese',
+    'jw': 'javanese',
+    'kn': 'kannada',
+    'kk': 'kazakh',
+    'km': 'khmer',
+    'ko': 'korean',
+    'ku': 'kurdish (kurmanji)',
+    'ky': 'kyrgyz',
+    'lo': 'lao',
+    'la': 'latin',
+    'lv': 'latvian',
+    'lt': 'lithuanian',
+    'lb': 'luxembourgish',
+    'mk': 'macedonian',
+    'mg': 'malagasy',
+    'ms': 'malay',
+    'ml': 'malayalam',
+    'mt': 'maltese',
+    'mi': 'maori',
+    'mr': 'marathi',
+    'mn': 'mongolian',
+    'my': 'myanmar (burmese)',
+    'ne': 'nepali',
+    'no': 'norwegian',
+    'or': 'odia',
+    'ps': 'pashto',
+    'fa': 'persian',
+    'pl': 'polish',
+    'pt': 'portuguese',
+    'pa': 'punjabi',
+    'ro': 'romanian',
+    'ru': 'russian',
+    'sm': 'samoan',
+    'gd': 'scots gaelic',
+    'sr': 'serbian',
+    'st': 'sesotho',
+    'sn': 'shona',
+    'sd': 'sindhi',
+    'si': 'sinhala',
+    'sk': 'slovak',
+    'sl': 'slovenian',
+    'so': 'somali',
+    'es': 'spanish',
+    'su': 'sundanese',
+    'sw': 'swahili',
+    'sv': 'swedish',
+    'tg': 'tajik',
+    'ta': 'tamil',
+    'te': 'telugu',
+    'th': 'thai',
+    'tr': 'turkish',
+    'uk': 'ukrainian',
+    'ur': 'urdu',
+    'ug': 'uyghur',
+    'uz': 'uzbek',
+    'vi': 'vietnamese',
+    'cy': 'welsh',
+    'xh': 'xhosa',
+    'yi': 'yiddish',
+    'yo': 'yoruba',
+    'zu': 'zulu',
+}
+
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/contactus')
+def contactus():
+    return render_template('contactus.html')
+
+# Function to handle text translation
+@app.route('/translate', methods=['GET', 'POST'])
+
+def translate_text():
+    translation = ""
+    text_to_translate = ""
+    target_lang = ""
+    src_lang = ""
+    
+   
+
+
+    if request.method == 'POST':
+        text_to_translate = request.form['text']
+        target_lang = request.form['target_lang']
+        src_lang = request.form['src_lang']
+        
+        
+        # Create a Translator object
+       
+        
+        translator = Translator()
+
+        # Perform the translation
+        try:
+            translated = translator.translate(text_to_translate, src=src_lang, dest=target_lang)
+            translation = translated.text
+        except Exception as e:
+            translation = f"Please choose the language above: {str(e)}"
+
+    # Render the template with the original text and the translation
+    return render_template(
+        'index.html', 
+        translation=translation, 
+        original_text=text_to_translate, 
+        target_lang=target_lang, 
+        src_lang=src_lang,
+        languages=LANGUAGES
+    )   
+   
+if __name__ == '__main__':
+    app.run(debug=True)
